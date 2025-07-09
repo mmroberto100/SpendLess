0a3cc4723505fe76c53667f4919c2dc3db029aaa
manuelrobertomunoz <mmroberto100@gmail.com>
2025-07-08 17:23:06 -0700

    Added convention plugins for core and auth modules

---
+++ build-logic/convention/src/main/java/com/mmunoz/convention/ExtensionType.kt
@@ -0,0 +1,6 @@
+package com.mmunoz.convention
+
+enum class ExtensionType {
+    APPLICATION,
+    LIBRARY
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,10 @@ dependencyResolutionManagement {
     }
 }
 
+gradle.startParameter.excludedTaskNames.addAll(listOf(":build-logic:convention:testClasses"))
+
 rootProject.name = "SpendLess"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 include(":app")
 include(":auth:data")
 include(":auth:domain")
